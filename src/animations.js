ed4fd05d2c180160c9e76c0c7348297873c3707d
pranjul jangra <pranjuljan68@gmail.com>
2025-02-03 18:34:24 +0530

    js file merged with gsap file

--- src/animations.js
+++ src/animations.js
@@ -221,3 +221,45 @@ const ticTacToe = gsap.timeline({
     }
 });
 ticTacToe.from('#tic-tac-toe', { y: 50, opacity: 0, duration: 0.4 });
+
+
+// Regular js code
+const navItems = document.querySelector('#nav-items');
+const homeSection = document.querySelector('#home');
+const aboutSection = document.querySelector('#about');
+const skillSection = document.querySelector('#skills');
+const projectSection = document.querySelector('#projects');
+
+function checkForScrollView(){
+    let halfWindowHeight = window.innerHeight / 2;
+
+    let homeBottom = homeSection.getBoundingClientRect().bottom;
+    let aboutTop = aboutSection.getBoundingClientRect().top;
+    let skillTop = skillSection.getBoundingClientRect().top;
+    let projectTop = projectSection.getBoundingClientRect().top;
+
+    if (homeBottom >= halfWindowHeight) {
+        navItems.style.setProperty('--active', '0');
+    } else if (aboutTop <= halfWindowHeight && skillTop > halfWindowHeight) {
+        navItems.style.setProperty('--active', '1');
+    } else if (skillTop <= halfWindowHeight && projectTop > halfWindowHeight) {
+        navItems.style.setProperty('--active', '2');
+    } else if (projectTop <= halfWindowHeight) {
+        navItems.style.setProperty('--active', '3');
+    }
+}
+checkForScrollView();
+
+window.addEventListener('scroll', () => {checkForScrollView()});
+
+navItems.addEventListener('click', (e)=> {
+    if (e.target.getAttribute('data-link') === 'home reference') {
+        homeSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else if (e.target.getAttribute('data-link') === 'about reference') {
+        aboutSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else if (e.target.getAttribute('data-link') === 'skills reference') {
+        skillSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else if(e.target.getAttribute('data-link') === 'projects reference'){
+        projectSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+});
