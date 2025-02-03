77eb000327bb6a8d19b01b9a593f5d15607310e0
pranjul jangra <pranjuljan68@gmail.com>
2025-02-03 18:17:46 +0530

    initial commit

---
+++ src/animations.js
@@ -0,0 +1,223 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+
+// HOME PAGE ANIMATIONS
+let homeTl = gsap.timeline();
+if(window.innerWidth >= 768){
+    homeTl.from('.profileImg', { x: () => window.innerWidth * 0.5 - 295, opacity: 0.7, duration: 0.7, delay: 0.9 })
+    .from('.profileName', { x: -200, opacity: 0, duration: 0.7, scale: 0.7 })
+    .from('.profileIntro', { x: -300, opacity: 0, duration: 0.7, scale: 0.5 }, '-=0.7');
+}else{
+    homeTl.from('.profileImg', { y: () => window.innerWidth * 0.5 - 245, opacity: 0.7, duration: 0.7, delay: 0.9 })
+    .from('.profileName', { x: -200, opacity: 0, duration: 0.7, scale: 0.7 })
+    .from('.profileIntro', { x: -300, opacity: 0, duration: 0.7, scale: 0.5 }, '-=0.7');
+}
+
+let logosTl = gsap.timeline();
+logosTl.to('#phoneLogo', { scale: 1, duration: 0.7, delay: 1.2, y: 0 })
+    .to('#emailLogo', { scale: 1, duration: 0.7, y: 0 }, '-=0.5')
+    .to('#linkedInLogo', { scale: 1, duration: 0.7, y: 0 }, '-=0.5')
+    .to('#githubLogo', { scale: 1, duration: 0.7, y: 0 }, '-=0.5');
+
+const navSvg = document.querySelector('.navSvg');
+const contactsList = document.querySelector('.contactsList');
+let isHovered = false;
+let swipeAnimation = null;
+
+//when we hovered over the svg while the contactList is already visible then,
+//  1. A new swipeAnimation is created and
+//  2. Previous swipeAnimation is already exists which results in creation of multiple animations
+//So we need to guard the animation to prevent multiple creation of animation.
+navSvg.addEventListener('mouseenter', () => {
+    isHovered = true;
+
+    //this will create an animation if it's not already exists and,
+    //'paused : true' property will pause the animation so that the outer 'play()' will trigger it
+    if (!swipeAnimation) {
+        if(window.innerWidth >= 1536){
+            swipeAnimation = gsap.to(contactsList, { top: '7rem', duration: 0.55, paused: true });
+        }else{
+            swipeAnimation = gsap.to(contactsList, { top: '4rem', duration: 0.55, paused: true });
+        }
+    }
+
+    swipeAnimation.timeScale(1).play(); // Play the existing animation
+});
+navSvg.addEventListener('mouseleave', () => { isHovered = false; checkIsHovered(); });
+contactsList.addEventListener('mouseenter', () => isHovered = true);
+contactsList.addEventListener('mouseleave', () => { isHovered = false; checkIsHovered(); });
+
+function checkIsHovered() {
+    setTimeout(() => {
+        if (!isHovered && swipeAnimation) swipeAnimation.timeScale(2).reverse();
+    }, 400)
+}
+
+
+// SCROLL TRIGGER ANIMATIONS:
+
+//It will creates an array of elements having class ".flowLeft"
+//.forEach is used to apply the scrollTrigger animations to each element
+//So that, they all will trigger independantly from each other.
+gsap.utils.toArray(".flowLeft").forEach(element => {
+    gsap.from(element, {
+        x: 200,
+        y: 10,
+        scale: 0.7,
+        opacity: 0.7,
+        scrollTrigger: {
+            trigger: element,  // Each element triggers its own animation
+            start: "top 99%",
+            end: "top 80%",
+            scrub: true,
+        }
+    });
+});
+
+gsap.utils.toArray(".moveUp").forEach(element => {
+    gsap.from(element, {
+        y: 50,
+        opacity: 0,
+        scale: 0.6,
+        scrollTrigger: {
+            trigger: element,
+            start: "top 99%",
+            end: "top 81%",
+            scrub: true,
+        }
+    });
+});
+
+// TIMELINE WITH SCROLL TRIGGER
+if(window.innerWidth >= 768){
+    //achievements (js test)
+    const jsTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#jsTest",
+            start: "top 80%",
+            end: "top 50%",
+            toggleActions: "play none none reverse",
+        }
+    });
+    jsTl.from('.jsIntro', { y: 80, opacity: 0, duration: 0.5 })
+        .from('.jsBadge', { y: 80, opacity: 0, duration: 0.5 }, '-=0.2')
+        .from('.jsProgress', { y: 80, opacity: 0, duration: 0.5 }, '-=0.2');
+
+    //achievements (html css test)
+    const htmlcssTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#htmlcssTest",
+            start: "top 80%",
+            end: "top 50%",
+            toggleActions: "play none none reverse",
+        }
+    });
+    htmlcssTl.from('.htmlcssIntro', { y: 80, opacity: 0, duration: 0.5 })
+        .from('.htmlcssBadge', { y: 80, opacity: 0, duration: 0.5 }, '-=0.2')
+        .from('.htmlcssProgress', { y: 80, opacity: 0, duration: 0.5 }, '-=0.2');
+
+}else{
+    gsap.utils.toArray('.animationBelow768').forEach(element =>{
+        gsap.from(element , {
+            y: 80,
+            opacity: 0,
+            duration: 0.5,
+            scrollTrigger:{
+                trigger: element,
+                start: 'top 80%',
+                end: 'top 60%',
+                toggleActions: 'play none none reverse'
+            }
+        })
+    })
+}
+
+
+//skills (frontend)
+const frontendTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#frontend",
+        start: "top 80%",
+        end: "top 50%",
+        toggleActions: "play none none reverse",
+    }
+});
+
+frontendTl.from('#html', { y: 50, opacity: 0, duration: 0.4 })
+    .from('#css', { y: 50, opacity: 0, duration: 0.4 }, '-=0.2')
+    .from('#sass', { y: 50, opacity: 0, duration: 0.4 }, '-=0.2')
+    .from('#tailwind', { y: 50, opacity: 0, duration: 0.4 }, '-=0.2')
+    .from('#javaScript', { y: 50, opacity: 0, duration: 0.4 }, '-=0.2')
+    .from('#reactjs', { y: 50, opacity: 0, duration: 0.4 }, '-=0.2')
+
+//skills (backend)
+const backendTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#backend",
+        start: "top 80%",
+        end: "top 50%",
+        toggleActions: "play none none reverse",
+    }
+});
+
+backendTl.from('#nodejs', { y: 50, opacity: 0, duration: 0.4 })
+    .from('#expressjs', { y: 50, opacity: 0, duration: 0.4 }, '-=0.2')
+    .from('#mongodb', { y: 50, opacity: 0, duration: 0.4 }, '-=0.2')
+
+//skills (other stacks)
+const otherStacksTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#otherStacks",
+        start: "top 80%",
+        end: "top 50%",
+        toggleActions: "play none none reverse",
+    }
+});
+
+otherStacksTl.from('#git', { y: 50, opacity: 0, duration: 0.4 })
+    .from('#github', { y: 50, opacity: 0, duration: 0.4 }, '-=0.2')
+    .from('#gsap', { y: 50, opacity: 0, duration: 0.4 }, '-=0.2')
+    .from('#vite', { y: 50, opacity: 0, duration: 0.4 }, '-=0.2')
+  
+//projects
+const clockTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#clock",
+        start: "top 80%",
+        end: "top 50%",
+        toggleActions: "play none none reverse",
+    }
+});
+clockTl.from('#clock', { y: 50, opacity: 0, duration: 0.4 });
+
+const calculatorTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#calculator",
+        start: "top 80%",
+        end: "top 50%",
+        toggleActions: "play none none reverse",
+    }
+});
+calculatorTl.from('#calculator', { y: 50, opacity: 0, duration: 0.4 });
+
+const rockPaperScissor = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#rock-paper-scissor",
+        start: "top 80%",
+        end: "top 50%",
+        toggleActions: "play none none reverse",
+    }
+});
+rockPaperScissor.from('#rock-paper-scissor', { y: 50, opacity: 0, duration: 0.4 });
+
+const ticTacToe = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#tic-tac-toe",
+        start: "top 80%",
+        end: "top 50%",
+        toggleActions: "play none none reverse",
+    }
+});
+ticTacToe.from('#tic-tac-toe', { y: 50, opacity: 0, duration: 0.4 });
--- src/script.js
+++ src/script.js
@@ -1,147 +1,39 @@
-//initial transitions
-const homeInfo = document.querySelector('#HomeInfo');
-const phone = document.querySelector('#phone');
-const email = document.querySelector('#email');
-const linkedIn = document.querySelector('#linkedIn');
-const github = document.querySelector('#github');
-const img = document.querySelector('#img');
-const intro = document.querySelector('#intro');
+const navItems = document.querySelector('#nav-items');
+const homeSection = document.querySelector('#home');
+const aboutSection = document.querySelector('#about');
+const skillSection = document.querySelector('#skills');
+const projectSection = document.querySelector('#projects');
 
-window.addEventListener('load', () => {
-    if (window.innerWidth >= 886) {
-        img.classList.add('translate-x-40');
-        intro.classList.add('translate-x-40');
-        homeInfo.classList.add('-translate-x-10');
-        setTimeout(() => {
-            img.classList.replace('opacity-0', 'opacity-1');
-            intro.classList.replace('opacity-0', 'opacity-1');
-        }, 400);
-        setTimeout(() => {
-            homeInfo.classList.replace('opacity-0', 'opacity-1');
-            homeInfo.classList.replace('-translate-x-10', 'translate-x-0');
-            img.classList.replace('translate-x-40', 'translate-x-0');
-            intro.classList.replace('translate-x-40', 'translate-x-0');
-        }, 600);
-    } else {
-        img.classList.add('translate-y-40');
-        intro.classList.add('translate-y-40');
-        homeInfo.classList.add('-translate-y-10');
-        setTimeout(() => {
-            img.classList.replace('opacity-0', 'opacity-1');
-            intro.classList.replace('opacity-0', 'opacity-1');
-        }, 400);
-        setTimeout(() => {
-            homeInfo.classList.replace('opacity-0', 'opacity-1');
-            homeInfo.classList.replace('-translate-y-10', 'translate-y-0');
-            img.classList.replace('translate-y-40', 'translate-y-0');
-            intro.classList.replace('translate-y-40', 'translate-y-0');
-        }, 600);
-    }
+function checkForScrollView(){
+    let halfWindowHeight = window.innerHeight / 2;
 
-    setTimeout(() => {
-        phone.classList.replace('scale-0', 'scale-1');
-    }, 1100);
-    setTimeout(() => {
-        email.classList.replace('scale-0', 'scale-1');
-    }, 1300);
-    setTimeout(() => {
-        linkedIn.classList.replace('scale-0', 'scale-1');
-    }, 1500);
-    setTimeout(() => {
-        github.classList.replace('scale-0', 'scale-1');
-    }, 1700);
-})
+    let homeBottom = homeSection.getBoundingClientRect().bottom;
+    let aboutTop = aboutSection.getBoundingClientRect().top;
+    let skillTop = skillSection.getBoundingClientRect().top;
+    let projectTop = projectSection.getBoundingClientRect().top;
 
-
-// scroller in navbar
-const home = document.querySelector('#home');
-const about = document.querySelector('#about');
-const skills = document.querySelector('#skills');
-const projects = document.querySelector('#projects');
-
-let navTabs = document.querySelector('#tabs');
-function checkSectionInView() {
-    const windowHeight = window.innerHeight;
-
-    let homeBottom = home.getBoundingClientRect().bottom;
-    let aboutTop = about.getBoundingClientRect().top;
-    let skillsTop = skills.getBoundingClientRect().top;
-    let projectsTop = projects.getBoundingClientRect().top;
-
-    if (homeBottom >= windowHeight / 2) {
-        navTabs.style.setProperty('--active', '0');
-    } else if (aboutTop <= windowHeight / 2 && aboutTop > -1) {
-        navTabs.style.setProperty('--active', '1');
-    } else if (skillsTop <= windowHeight / 2 && skillsTop > -1) {
-        navTabs.style.setProperty('--active', '2');
-    } else if (projectsTop <= windowHeight / 2 && projectsTop > -1) {
-        navTabs.style.setProperty('--active', '3')
+    if (homeBottom >= halfWindowHeight) {
+        navItems.style.setProperty('--active', '0');
+    } else if (aboutTop <= halfWindowHeight && skillTop > halfWindowHeight) {
+        navItems.style.setProperty('--active', '1');
+    } else if (skillTop <= halfWindowHeight && projectTop > halfWindowHeight) {
+        navItems.style.setProperty('--active', '2');
+    } else if (projectTop <= halfWindowHeight) {
+        navItems.style.setProperty('--active', '3');
     }
 }
-checkSectionInView();
-window.addEventListener('scroll', checkSectionInView);
+checkForScrollView();
 
+window.addEventListener('scroll', () => {checkForScrollView()});
 
-const navitems = document.querySelectorAll('.nav-items');
-
-for (let item of navitems) {
-    item.addEventListener('click', () => {
-        if (item.textContent == 'home') {
-            home.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        } else if (item.textContent == 'about') {
-            about.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        } else if (item.textContent == 'skills') {
-            skills.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        } else if (item.textContent == 'projects') {
-            projects.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
-    })
-}
-
-
-//contacts list swiping
-const contactRef = document.querySelector('#contactRef');
-const contactList = document.querySelector('#contactsList');
-let isContactRefHovered = false;
-let isContactListHovered = false;
-
-setTimeout(() => {
-    contactList.classList.remove('opacity-0');
-}, 400);
-
-if (window.innerWidth < 2000) {
-    contactList.classList.add('-translate-y-72');
-} else {
-    contactList.classList.add('-translate-y-[26rem]');
-}
-
-contactRef.addEventListener('mouseenter', () => {
-    if (contactList.classList.contains('-translate-y-72')) {
-        contactList.classList.replace('-translate-y-72', 'translate-y-0');
-    } else {
-        contactList.classList.replace('-translate-y-[26rem]', 'translate-y-10');
+navItems.addEventListener('click', (e)=> {
+    if (e.target.getAttribute('data-link') === 'home reference') {
+        homeSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else if (e.target.getAttribute('data-link') === 'about reference') {
+        aboutSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else if (e.target.getAttribute('data-link') === 'skills reference') {
+        skillSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else if(e.target.getAttribute('data-link') === 'projects reference'){
+        projectSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
-    isContactRefHovered = true;
 });
-contactRef.addEventListener('mouseleave', () => {
-    isContactRefHovered = false;
-    contactScrollsUp();
-});
-contactList.addEventListener('mouseenter', () => {
-    isContactListHovered = true;
-});
-contactList.addEventListener('mouseleave', () => {
-    isContactListHovered = false;
-    contactScrollsUp();
-});
-function contactScrollsUp() {
-    setTimeout(() => {
-        if (!isContactRefHovered && !isContactListHovered) {
-            if (window.innerWidth < 2000) {
-                contactList.classList.replace('translate-y-0', '-translate-y-72');
-            } else {
-                contactList.classList.replace('translate-y-10', '-translate-y-[26rem]')
-            };
-        };
-    }, 200);
-};
